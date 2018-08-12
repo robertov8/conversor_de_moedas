62654a23a5c5411689ea4fdb50aa36a6b21fa986
Roberto Ribeiro <robertov82008@gmail.com>
2018-08-11 22:40:53 -0300

    48 Trabalhando com Futuro

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = 'https://api.hgbrasil.com/finance?format=json&key=60df7606';
+
+void main() async {
+  runApp(MaterialApp(
+    title: 'Conversor de Moedas',
+    home: Home(),
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('\$ Conversor de Moedas \$'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
+
+
