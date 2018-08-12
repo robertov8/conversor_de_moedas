38d3ba92adcc0981d77255b65e3feb39a90ceeba
Roberto Ribeiro <robertov82008@gmail.com>
2018-08-11 23:42:36 -0300

    52 - Realizando a Conversão

--- lib/main.dart
+++ lib/main.dart
@@ -29,16 +29,26 @@ class _HomeState extends State<Home> {
   final dolarController = TextEditingController();
   final euroController = TextEditingController();
 
-  double dollar;
+  double dolar;
   double euro;
 
   void _realChanged(String text) {
-    print(text);
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
   }
 
-  void _dolarChanged(String text) {}
+  void _dolarChanged(String text) {
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
 
-  void _euroChanged(String text) {}
+  void _euroChanged(String text) {
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,7 +80,7 @@ class _HomeState extends State<Home> {
                   textAlign: TextAlign.center,
                 ));
               } else {
-                dollar = snapshot.data['results']['currencies']['USD']['buy'];
+                dolar = snapshot.data['results']['currencies']['USD']['buy'];
                 euro = snapshot.data['results']['currencies']['EUR']['buy'];
 
                 return SingleChildScrollView(
